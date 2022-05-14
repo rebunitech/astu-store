08bcbedc5c899c15e55d139c87d9da31a22340d8
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2022-05-15 00:25:18 +0300

    [feat] mixins --> for captures activities of user and permissions                       user view  --> activity of logged user like editProfile, change password, change permsission                      forms  --> provides input form

---
+++ src/store_mngt/auser/forms.py
@@ -0,0 +1,63 @@
+"""gradient_infosys auser URL Configuration
+	
+    Created by: Wendirad Demelash
+    Last modified by: Wendirad Demelash
+"""
+
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import (AuthenticationForm, UserCreationForm,
+                                       UsernameField)
+from django.contrib.auth.models import Permission
+from django.utils.translation import gettext_lazy as _
+
+from auser.models import EducationBackground, Student, Experience
+
+UserModel = get_user_model()
+
+
+class UserChangeForm(forms.ModelForm):
+    """Model form used to update user profile"""
+
+    class Meta:
+        model = UserModel
+        fields = (
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "sex",
+            "location",
+            "po_box",
+            "profile_picture",
+            "bio",
+        )
+        field_classes = {"username": UsernameField}
+
+
+class LoginForm(AuthenticationForm):
+    """Form used to login"""
+
+    class Meta:
+        labels = {"username", _("Username or email")}
+
+
+class ChangePermissionForm(forms.ModelForm):
+    """Form used to change user's permission"""
+
+    user_permissions = forms.ModelMultipleChoiceField(
+        queryset=Permission.objects.all(), required=False
+    )
+
+    class Meta:
+        model = UserModel
+        fields = ("user_permissions",)
+
+
+# class StudentRegistrationForm(UserCreationForm):
+#     class Meta(UserCreationForm.Meta):
+#         model = Student
+#         fields = ("username", "email", "first_name", "last_name", "phone_number", "sex")
+
+
