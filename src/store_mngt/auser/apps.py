57f4bca8d2d340131361d751e6b093b9c676af0c
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2022-05-15 00:31:15 +0300

    [feat] backends file used for email

--- src/store_mngt/auser/apps.py
+++ src/store_mngt/auser/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class AuserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auser'
+
+    def ready(self):
+        from auser.signals import create_permission_groups
+
+        post_migrate.connect(create_permission_groups, sender=self)
