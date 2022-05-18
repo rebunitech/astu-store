62bb075a2b1bac119f044860ed3de2f90c222abb
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2022-05-18 17:18:10 +0300

    [fix] school, department, storekeeper is added to admin site.

--- src/store_mngt/auser/admin.py
+++ src/store_mngt/auser/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Department, School, Storekeeper
+
+admin.site.register(Department)
+admin.site.register(School)
+admin.site.register(Storekeeper)
