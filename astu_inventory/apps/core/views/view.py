161428b6f69a6164ad84a08853a64e93cad6b112
Ashebir07 <ashu07wondemeneh@gmail.com>
2022-07-10 01:06:39 +0300

    [feat] adding core app

---
+++ astu_inventory/apps/core/views/view.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.db.models import F, Q, Sum
+from django.db.models.functions import Coalesce
+from django.views.generic import ListView
+
+from astu_inventory.apps.inventory.models import Product
+
+
+class ListAvailableProductsView(PermissionRequiredMixin, ListView):
+    model = Product
+    context_object_name = "products"
+    permission_required = "view_product"
+    extra_context = {"title": "Available Products"}
+    template_name = "core/product_list.html"
+
+    # def get_queryset(self):
+    #     qs = (
+    #         super()
+    #         .get_queryset()
+    #         .annotate(
+    #             total_item=Coalesce(Sum("items__quantity"), 0),
+    #             total_request=Coalesce(
+    #                 Sum(
+    #                     "borrow_requests__quantity",
+    #                     filter=Q(borrow_requests__status=1),
+    #                 ),
+    #                 0,
+    #             ),
+    #         )
+    #         .annotate(total_availables=F("total_item") - F("total_request"))
+    #         # .filter(Q(total_availables__gt=0))
+    #     )
+    #     return qs
